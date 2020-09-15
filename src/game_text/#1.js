da24004c3a5d0ecd2e3ee400fc36cf7d4feb3f06
marcoulakis <femarcoulakis@gmail.com>
2020-09-15 17:06:14 -0300

    published the game page desing

--- src/game_text/#1.js
+++ src/game_text/#1.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import {Link} from "react-router-dom";
-import ena from './game_text/#1'
-import "./css/main.css";
-import "./css/game_index.css";
+import {Link} from "react-router-dom"; 
+import "../css/main.css";
+import "../css/game_index.css";
 
 
 
-function Game(){
+function ena(){
+
+
+
 
     return(
         <div className="all">
             <div className="all_game">
                 <div className="name"></div>
                 <div className="textArea"></div>
-                <Link to={ena}>
+                <Link to="">
                     <div id="n1" className="Next">
                         <p className="next">Próximo</p>
                     </div>
@@ -27,4 +29,4 @@ function Game(){
 
 
 
-export default Game;
+export default ena;
