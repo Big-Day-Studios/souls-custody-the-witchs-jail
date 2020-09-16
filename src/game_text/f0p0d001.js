043f1d271f39aa462ff3569050acd637574b97c8
marcoulakis <femarcoulakis@gmail.com>
2020-09-15 21:39:42 -0300

    Created the game router

---
+++ src/game_text/f0p0d001.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Game from "../game";
+import {Link} from "react-router-dom";
+
+
+
+export default class ena extends React.Component{
+    render(){    
+        return(
+            <div className= "all">
+                <Game />
+                <Link to="f0p0d002">
+                    <div id="n1" className="Next">
+                        <p className="next">Próximo</p>
+                    </div>
+                </Link>
+            </div>
+        );
+    }
+}
+
+
+
